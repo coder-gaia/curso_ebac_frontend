b11d89af3ba29c1559041798766d421665122d78
Alexandre Gaia <132020885+coder-gaia@users.noreply.github.com>
2023-12-08 19:11:58 -0300

    Add files via upload

---
+++ exercise.js
@@ -0,0 +1,9 @@
+"use strict";
+function multplication(num1, num2) {
+    return num1 * num2;
+}
+console.log(`The multiplication result is: ${multplication(9, 9)}`);
+function sayHello(name) {
+    return `hello ${name}, how've u been these days?`;
+}
+console.log(sayHello('alexandre'));
---
+++ exercise.ts
@@ -0,0 +1,9 @@
+function multplication(num1: number, num2: number): number{
+    return num1 * num2
+}
+console.log(`The multiplication result is: ${multplication(9, 9)}`)
+
+function sayHello(name:string):string{
+    return `hello ${name}, how've u been these days?`
+}
+console.log(sayHello('Alexandre'))
